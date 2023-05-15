62fb019cacb3c28f2d34c3792d49da193978f1d7
snp43 <118197911+snp43@users.noreply.github.com>
2023-05-15 16:09:05 -0400

    2fa implemented

--- web/src/pages/2fa.js
+++ web/src/pages/2fa.js
@@ -1,14 +1,41 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
 import Link from "next/link";
 import { useState } from "react";
+import speakeasy from "speakeasy";
+import emailjs from 'emailjs-com';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userToken, setUserToken] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoginActive, setIsLoginActive] = useState(false);
+  const email = "snp43@cornell.edu"; // replace with the actual email
+
+  const generateToken = () => {
+    const newToken = speakeasy.generateSecret().base32;
+    setToken(newToken);
+
+    emailjs.send("service_adphn8k", "template_bcnrzfa", {
+      username: email,
+      token: newToken,
+    }, "kHQONyhpLT-m20lgu")
+    .then(() => setMessage("Token sent! Check your email."))
+    .catch(() => setMessage("Error sending token."));
+  };
+
+  const checkToken = () => {
+    if (userToken === token) {
+      setIsLoginActive(true);
+      setMessage("Token confirmed. You can now log in.");
+    } else {
+      setMessage("Incorrect token. Please try again.");
+    }
+};
   return (
     <>
       <div className="min-h-screen bg-gray-100">
@@ -52,60 +79,27 @@ export default function Home() {
               </p>
             </div>
           </section>
-
-          {/* <form> */}
-          <div className="flex items-center justify-center bg-gray-100">
-            <div className="bg-white shadow-md rounded-lg p-8 w-1/4">
-              <h2 className="text-blue-500 text-7xl font-semibold py-2 px-4 mb-6 text-center">
-                Log In
-              </h2>
-              <form>
-                <div className="mb-4">
-                  <label
-                    className="block text-gray-700 text-3xl font-bold mb-2"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="shadow appearance-none border rounded w-full py-4 px-5 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="email"
-                    placeholder="Applicant's Email"
-                  />
-                </div>
-                <div className="mb-6">
-                  <label
-                    className="block text-gray-700 text-3xl font-bold mb-2"
-                    htmlFor="password"
-                  >
-                    Password
-                  </label>
-                  <input
-                    className="shadow appearance-none border rounded w-full py-4 px-5 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    type="password"
-                    placeholder="Applicant's Password"
-                  />
-                </div>
-                <div className="flex items-center justify-between ">
-                  <button
-                    className="bg-blue-500 text-2xl hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
-                    type="button"
-                  >
-                    <Link href="/2fa">Log In</Link>
-                  </button>
-
-                  <Link
-                    href="/"
-                    className="inline-block align-baseline font-bold text-2xl text-blue-500 hover:text-blue-800"
-                  >
-                    Forgot Password?
-                  </Link>
-                </div>
-              </form>
-            </div>
-          </div>
-          {/* </form> */}
+          <div className="text-center">
+        {token === null && (
+          <button onClick={generateToken}>Generate Token</button>
+        )}
+        {token !== null && (
+          <>
+            <input
+              type="text"
+              placeholder="Enter your token"
+              onChange={(e) => setUserToken(e.target.value)}
+            />
+            <button onClick={checkToken}>Verify Token</button>
+            {isLoginActive && (
+              <Link href="/index_applicant">
+                <button>Login</button>
+              </Link>
+            )}
+          </>
+        )}
+        {message && <p>{message}</p>}
+      </div>
         </main>
 
         <footer className="bg-gray-200 p-6">
